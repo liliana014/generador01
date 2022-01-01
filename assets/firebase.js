15000c6d36e72681703f39ba29a825efaffe9fce
liliana014 <93648399+liliana014@users.noreply.github.com>
2022-01-01 15:11:27 -0500

    Add files via upload

---
+++ assets/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDu4xsIBsoMUJ_kd3qnnPVCb6gwV5QaDNo",
+  authDomain: "autentificacion-60f25.firebaseapp.com",
+  databaseURL: "https://autentificacion-60f25-default-rtdb.firebaseio.com",
+  projectId: "autentificacion-60f25",
+  storageBucket: "autentificacion-60f25.appspot.com",
+  messagingSenderId: "206130119070",
+  appId: "1:206130119070:web:beac7daf89a3835be9968f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
